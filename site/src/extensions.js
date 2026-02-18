94bc1bc6a16a6f6e68e64cc898ce2e6dbfd8591e
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2026-02-17 22:11:38 -0500

    "The Artimus Flsamethrower tool" - DizzyAwe

---
+++ site/src/extensions.js
@@ -0,0 +1,14 @@
+editor.addExtension = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(res => res.text()).then(text => {
+            try {
+                const extensionJSON = JSON.parse(text);
+                extensionJSON.url = url;
+                
+                //Add it
+                editor.settings.extensions.push(extensionJSON);
+                resolve(extensionJSON);
+            } catch (error) { console.warn(`Failed to add extension ${url}\n::===---===::\n${error}`) }
+        });
+    });
+}
--- site/src/settingsPages/extensionPage.js
+++ site/src/settingsPages/extensionPage.js
@@ -1,4 +1,5 @@
 editor.extensionMenu = (container, translationKey, onchange) => {
+    //So much, dom, manipulation...
     const additionHolder = document.createElement("div");
     additionHolder.className = "settings-extensionDisplay settings-extensionAdditionDisplay";
 
@@ -9,12 +10,59 @@ editor.extensionMenu = (container, translationKey, onchange) => {
     const extensionAdd = document.createElement("button");
     extensionAdd.className = "artimus-button settings-extensionAdd";
     extensionAdd.innerText = artimus.translate("add", translationKey);
+    
+    const extensionList = document.createElement("div");
+    extensionList.className = "settings-extensionList";
 
     const addExtensionDisplay = (extension) => {
+        console.log(extension);
+        const extensionHolder = document.createElement("div");
+        extensionHolder.className = "settings-extensionHolder";
 
+        const extensionData = document.createElement("div");
+        extensionData.className = "settings-extensionData";
+
+        const extensionName = document.createElement("div");
+        extensionName.className = "settings-extensionDescription settings-extensionName";
+        extensionName.innerText = extension.name || artimus.translate("namePlaceholder", translationKey);
+
+        const extensionDescription = document.createElement("div");
+        extensionDescription.className = "settings-extensionDescription";
+
+        //Description stuff
+        extensionDescription.innerText = artimus.translate("description", translationKey)
+        .replace("[author]", (extension.author || artimus.translate("authorPlaceholder", translationKey)))
+        .replace("[version]", (extension.version || artimus.translate("versionPlaceholder", translationKey)));
+
+        //The finishing touch.
+        const extensionModContainer = document.createElement("div");
+        extensionModContainer.className = "settings-extensionModContainer";
+    
+        const extensionRemove = document.createElement("button");
+        extensionRemove.className = "artimus-button settings-extensionAdd settings-extensionRemove";
+        extensionRemove.innerText = artimus.translate("remove", translationKey);
+
+        extensionData.appendChild(extensionName);
+        extensionData.appendChild(extensionDescription);
+        extensionModContainer.appendChild(extensionRemove);
+        extensionHolder.appendChild(extensionData);
+        extensionHolder.appendChild(extensionModContainer);
+        extensionList.appendChild(extensionHolder);
+    }
+
+    extensionAdd.onclick = () => {
+        editor.addExtension(urlInput.value).then((extension) => {
+            addExtensionDisplay(extension);
+            onchange();
+        })
+    }
+
+    for (let extID in editor.settings.extensions) {
+        addExtensionDisplay(editor.settings.extensions[extID]);
     }
 
     additionHolder.appendChild(urlInput);
     additionHolder.appendChild(extensionAdd);
     container.appendChild(additionHolder);
+    container.appendChild(extensionList);
 }
