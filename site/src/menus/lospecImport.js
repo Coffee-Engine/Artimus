be8279e5d4103b4aecc15467e540bae8af7f271c
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2026-04-12 10:49:10 -0400

    Continue palette menu work.

--- site/src/menus/lospecImport.js
+++ site/src/menus/lospecImport.js
@@ -3,7 +3,8 @@ editor.lospecMenu = () => {
         //Create the search box.
         const input = document.createElement("input");
         input.type = "text";
-        input.placeholder = artimus.translate("lospec.title", "modal");
+        input.placeholder = artimus.translate("lospec.placeholder", "modal");
+        input.className = "lospec-paletteInput";
 
         //Make the display for the palette
         const previewContainer = document.createElement("div");
@@ -14,13 +15,13 @@ editor.lospecMenu = () => {
         paletteInformation.className = "lospec-previewInformation";
 
         const paletteName = document.createElement("p");
-        paletteName.className = "lospec-previewGame";
+        paletteName.className = "lospec-information lospec-previewName";
 
         const colorCount = document.createElement("p");
-        colorCount.className = "lospec-previewCount";
+        colorCount.className = "lospec-information lospec-previewCount";
 
         const authorName = document.createElement("p");
-        authorName.className = "lospec-previewAuthor";
+        authorName.className = "lospec-information lospec-previewAuthor";
 
         // Color preview
         const paletteDisplay = document.createElement("div");
@@ -29,9 +30,9 @@ editor.lospecMenu = () => {
         //Add information
         paletteInformation.appendChild(paletteName);
         paletteInformation.appendChild(colorCount);
-        paletteInformation.appendChild(authorName);
 
         previewContainer.appendChild(paletteInformation);
+        previewContainer.appendChild(authorName);
         previewContainer.appendChild(paletteDisplay);
 
         content.appendChild(input);
@@ -40,13 +41,15 @@ editor.lospecMenu = () => {
         const paletteJSONRetrieved = (text) => {
             const json = JSON.parse(text);
 
-            const name = json.name || "palette";
-            const author = json.author || "author";
-            const colors = (json.colors || ["#ff00ff"]).map((val) => `#${val}`);
+            //Parse the text and make sure to add placeholders if the need is there.
+            const name = json.name || artimus.translate("lospec.placeholder.name", "modal");
+            const author = json.author || artimus.translate("lospec.placeholder.author", "modal");
+            const colors = (json.colors || ["ff00ff", "000000"]).map((val) => `#${val}`);
 
+            //Add the text;
             paletteName.innerText = name;
-            colorCount.innerText = colors.length;
-            authorName.innerText = author;
+            colorCount.innerText = artimus.translate("lospec.colors", "modal").replace("[count]", colors.length);
+            authorName.innerText = artimus.translate("lospec.by", "modal").replace("[author]", author);
 
             //Figure out the palette display;
             if (colors.length == 1) paletteDisplay.style.setProperty("--palette", colors[0]);
@@ -67,16 +70,20 @@ editor.lospecMenu = () => {
             }
         }
 
+        //Just init.
+        paletteJSONRetrieved("{}");
+
         //Add functionality
         input.onchange = () => {
-            let paletteID = input.value.toLowerCase().replaceAll(/\s/g, "-");
+            let paletteID = input.value.toLowerCase().trim().replaceAll(/\s/g, "-");
             if (paletteID.endsWith("palette")) paletteID = paletteID.replace(/-?palette/, "");
 
             //Fetch the palette from the lospec api, both before and api palette
             fetch(`https://Lospec.com/palette-list/${paletteID}.json`).then(res=>res.text())
             .then(paletteJSONRetrieved)
             .catch(() => {
-                console.warn(`Palette "${paletteID}" can't be found!`)
+                console.warn(`Palette "${paletteID}" can't be found!`);
+                paletteJSONRetrieved("{}");
             });
         }
     }, { height: 30 });
