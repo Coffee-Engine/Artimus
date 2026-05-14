40d3cea7e98693db7dbc47728e053503f021ada9
Obvious Scraptop <71141838+AlexSchoolOH@users.noreply.github.com>
2026-05-14 08:57:24 -0400

    Swatch

--- site/lib/elemental/elementalCUGIBridge.js
+++ site/lib/elemental/elementalCUGIBridge.js
@@ -11,6 +11,7 @@ if (window.CUGI) {
         input.gradient = (typeof data.gradient == "function") ? data.gradient() : data.gradient;
         input.disabled = (typeof data.disabled == "function") ? data.disabled() : data.disabled;
         input.forCanvas = (typeof data.disabled == "function") ? data.forCanvas() : data.forCanvas;
+        input.swatch = (typeof data.swatch == "function") ? data.swatch() : data.swatch;
 
         input.onchange = CUGI.macros.onchange(data, input);
 
