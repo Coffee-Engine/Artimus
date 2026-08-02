dfdb0fc3355556c2da19048ffed141cd0eb4000c
Obvious Scraptop <71141838+AlexSchoolOH@users.noreply.github.com>
2026-08-01 21:47:16 -0400

    update version number

--- site/src/main.js
+++ site/src/main.js
@@ -1,5 +1,5 @@
 window.editor = {
-    version: "Γ 1.5.1",
+    version: "Γ 1.5.2",
     bannerTitle: "Welcome Artimus!",
     bannerAuthor: "BitDisco",
     bannerAuthorURL: "https://bitdisco.itch.io/",
